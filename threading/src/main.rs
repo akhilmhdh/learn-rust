53dcacd537df572f78f14763651c8f987fa78f87
akhilmhdh <akhilmhdh@gmail.com>
2021-04-05 20:10:39 +0530

    feat: channel communication completed

--- threading/src/main.rs
+++ threading/src/main.rs
@@ -1,5 +1,30 @@
+use std::io;
+
 mod basic_threading;
+mod channel;
 
 fn main() {
-    basic_threading::main();
+    loop {
+        let mut input = String::new();
+
+        println!(
+            "
+Threading
+1. Basic Threading
+2. Channel
+0. Exit
+        "
+        );
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Program number must be given");
+
+        let input: i32 = input.trim().parse().expect("To be a number");
+        match input {
+            0 => break,
+            1 => basic_threading::main(),
+            2 => channel::main(),
+            _ => println!("invalid option"),
+        }
+    }
 }
